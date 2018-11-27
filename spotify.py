1fc0ccd56758e35d2ec5a5626019073fd2ac8a84
Grace Z <gz.gracez16@gmail.com>
2018-11-27 14:55:00 -0500

    some spotify py

---
+++ spotify.py
@@ -0,0 +1,38 @@
+# import sys
+# import spotipy
+# import spotipy.util as util
+
+# scope = 'user-library-read'
+
+# if len(sys.argv) > 1:
+#     username = sys.argv[1]
+# else:
+#     print("Usage: %s username" % (sys.argv[0],))
+#     sys.exit()
+# os.environ['DEBUSSY']
+# # username = 'gzgracez2'
+
+# token = util.prompt_for_user_token(username, scope)
+
+# if token:
+#     sp = spotipy.Spotify(auth=token)
+#     results = sp.current_user_saved_tracks()
+#     for item in results['items']:
+#         track = item['track']
+#         print(track['name'] + ' - ' + track['artists'][0]['name'])
+# else:
+#     print("Can't get token for", username)
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
